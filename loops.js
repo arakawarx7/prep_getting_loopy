eae3752b37b7dc0d68cde11ec2f381a53cfb75c9
Aaron Arakawa <Aaron Arakawa>
2016-05-10 21:32:07 -1000

    complete exercise 1

--- loops.js
+++ loops.js
@@ -1,1 +1,20 @@
-// Do your work here
+var presidents =['Washington','Adams','Jefferson','Madison','Monroe'];
+	for (var i =0; i < presidents.length; i++){
+		console.log('The value of i is ' + presidents[i]);
+}		console.log('The value of index is ' + i);
+console.log('\nbonus\n');
+
+var presidents1 =['Washington','Adams','Jefferson','Madison','Monroe'];
+function printContent(presidents,lastNames){
+	for (var i =0; i < presidents1.length; i++){
+		console.log(presidents1[i]);
+}		
+
+}
+printContent(presidents1);
+
+var stringOfNumbers = '';
+	for(i =10; i<=20;i++){
+
+	console.log(i);
+}
